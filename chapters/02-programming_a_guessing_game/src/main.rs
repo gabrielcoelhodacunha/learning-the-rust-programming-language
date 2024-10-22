cf49d5f785d71aa6978d21a563f6c46fc73f11c1
Gabriel Coelho da Cunha <43345507+gabrielcoelhodacunha@users.noreply.github.com>
2024-10-22 11:41:43 -0300

    Break Chapter 2 code into more functions

--- chapters/02-programming_a_guessing_game/src/main.rs
+++ chapters/02-programming_a_guessing_game/src/main.rs
@@ -1,35 +1,41 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::io::{self, Write};
+use std::num::ParseIntError;
 
 fn main() {
-    println!("Guess the number!");
+    let secret_number = generate_secret_number();
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
+    println!("Welcome to GUESS THE NUMBER!");
     loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match read_guess() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
-
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{guess} is too small!"),
+            Ordering::Greater => println!("{guess} is too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
     }
+}
+
+fn generate_secret_number() -> u32 {
+    return rand::thread_rng().gen_range(1..=100);
+}
+
+fn read_guess() -> Result<u32, ParseIntError> {
+    let mut guess = String::new();
+
+    print!("What is your guess? ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    return guess.trim().parse();
 }
